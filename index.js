3e1f402b7062ce43e78fb224f0f716d091d82b5c
Henry-Bernardin <henry.bernardin@gmail.com>
2020-02-13 11:19:21 -0500

    Bottom Wall created

--- index.js
+++ index.js
@@ -21,5 +21,6 @@ World.add(world, shape);
 //Walls
 const walls = [
     Bodies.rectangle(400, 0, 800, 40, {isStatic:true}), 
+    Bodies.rectangle(400, 600, 800, 40, {isStatic:true}), 
 ];
 World.add(world, walls);
