878f5cd545b85e6adcace1adb342633431f25d88
Henry-Bernardin <henry.bernardin@gmail.com>
2020-03-12 23:39:25 -0400

    Rectangle: static to false

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ World.add(world, MouseConstraint.create(engine, {
 }));
 
 const shape = Bodies.rectangle(200, 200, 50, 50, {
-    isStatic: true
+    isStatic: false
 }); 
 World.add(world, shape);
 
