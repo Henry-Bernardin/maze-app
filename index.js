7adf1b959debd70b16d9dc9d40c0b6680a83bda2
Henry-Bernardin <henry.bernardin@gmail.com>
2020-04-11 20:48:10 -0400

    created const stepThroughWall

--- index.js
+++ index.js
@@ -49,3 +49,17 @@ const horizontals = Array(cells - 1)
 
 const startRow = Math.floor(Math.random() * cells); 
 const startColumn - Math.floor(Math.random() * cells); 
+
+const stepThroughCell = (row, column) => {
+    // If I have visited the cell at [row, column], then return 
+
+    // Mark this cell as being visited 
+
+    // Assemble randomly-ordered list of neighbors
+
+    // For each neighbor...
+
+    //See if that neighbor is out of bounds
+
+    // If we have visited that neighbor continue to next neighbor
+}; 
