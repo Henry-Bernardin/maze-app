3915403b5103b8740fe884f09d60ffdf3699eca5
Henry-Bernardin <henry.bernardin@gmail.com>
2020-04-29 18:03:06 -0400

    Set velocity for keycode 83(S)

--- index.js
+++ index.js
@@ -189,7 +189,7 @@ if (event.keyCode === 68){
 }
     // S - Move ball down
 if (event.keyCode === 83){
-    
+    Body.setVelocity(ball, {x, y: y + 5}); 
 }
     // A - Move ball left
 if (event.keyCode === 65){
